ce3818ae79333de98aafed7bf9aa72e3ed09b7f5
YunhoChoi <choiking10@naver.com>
2021-01-12 19:32:19 +0900

    Fix enumerator bug. Fix layer count mismatch.

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class VGG(nn.Module):
 
         self.conv1 = self.create_layer([3, 64, 64])
         self.conv2 = self.create_layer([64, 128, 128])
-        self.conv3 = self.create_layer([128, 256, 256, 256])
+        self.conv3 = self.create_layer([128, 256, 256, 256, 256])
         self.conv4 = self.create_layer([256, 512, 512, 512, 512])
         self.conv5 = self.create_layer([512, 512, 512, 512, 512])
 
@@ -31,7 +31,9 @@ class VGG(nn.Module):
     def create_layer(self, param):
         layer = []
         start = param[0]
-        for i, f in enumerate(param, 1):
+        iter = enumerate(param)
+        next(iter)
+        for i, f in iter:
             layer.append((f"c{i-1}", nn.Conv2d(start, f, kernel_size=3, padding=1)))
             layer.append((f"r{i-1}", nn.ReLU(inplace=True)))
             start = f
